85863cd0d5a9b0998ff7fef14533e38d94fa8d98
brody-rebne <bsrebne@gmail.com>
2020-02-18 10:32:39 -0800

    added index file

--- server.js
+++ server.js
@@ -7,3 +7,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 const cors = require('cors');
 app.use(cors);
+
+const PORT = process.env.PORT || 3003;
+
+app.use(express.static('./public'));
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
