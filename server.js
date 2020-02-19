25f3c34c9caff5171ef0c120089d86d001004bb3
brody-rebne <bsrebne@gmail.com>
2020-02-19 11:16:09 -0800

    accessing darksky api

--- server.js
+++ server.js
@@ -13,27 +13,23 @@ const PORT = process.env.PORT || 3003;
 app.get('/location', (request, response) => {
   let city = request.query.city;
   let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATIONIQ_API}&q=${city}&format=json`;
-  superagent.get(url).then(results => {
+  superagent.get(url).then(result => {
     let city = request.query.city;
-    let geoData = results.body;
+    let geoData = result.body;
     let location = new City(city, geoData[0]);
     response.send(location);
   }).catch(err => console.error(err));
 });
 
 app.get('/weather', (request, response) => {
-  try {
-    let darksky = require('./data/darksky.json');
-    let forecast = darksky.daily.data.map((fc) => new WeatherDay(fc));
-    console.log(forecast);
-    // for(let i=0;i<darksky.daily.data.length;i++) {
-    //   let dailyWeather = new WeatherDay(darksky, i);
-    //   forecast.push(dailyWeather);
-    // }
+  let lat = request.query.latitude;
+  let lon = request.query.longitude;
+  let url = `https://api.darksky.net/forecast/${process.env.DARKSKY_API}/${lat},${lon}`;
+  superagent.get(url).then(result => {
+    console.log(result.body.daily.data[0]);
+    let forecast = result.body.daily.data.map((fc) => new WeatherDay(fc));
     response.send(forecast);
-  } catch (err) {
-    response.status(500).send(`${err}`);
-  }
+  }).catch(err => console.error(err));
 });
 
 function City(city, obj) {
@@ -45,7 +41,7 @@ function City(city, obj) {
 
 function WeatherDay(obj) {
   this.forecast = obj.summary;
-  let date = new Date(obj.time);
+  let date = new Date(obj.time * 1000);
   this.time = date.toDateString();
 }
 
